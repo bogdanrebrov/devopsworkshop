13155b7db5be92829b9e3dc5b71169402325b2fd
bogdan.rebrov <bogdan.rebrov@dev-pro.net>
2019-03-23 22:15:32 +0200

    added sh script

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ sudo add-apt-repository ppa:longsleep/golang-backports -y
 sudo apt-get update -y
 sudo apt-get install golang-go -y
 sudo apt install postgresql-client -y
+sudo apt install go-dep -y
